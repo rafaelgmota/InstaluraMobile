f01cddee25c039ee3371aaa155a077753e70f0f4
Rafael Mota <faelmota@gmail.com>
2018-01-18 20:30:44 -0300

    Extracting Post Component

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React from 'react';
 import {
-    Text,
-    View,
-    Image,
-    Dimensions,
     FlatList,
     StyleSheet,
 } from 'react-native';
+import Post from "./src/components/Post";
 
 const photos = [
     {id: 1, user: 'Rafael'},
     {id: 2, user: "Jorge"},
     {id: 3, user: 'Irineu'},
 ];
-
-const {width} = Dimensions.get('screen');
 
 export default class App extends React.Component {
     render() {
@@ -29,15 +18,7 @@ export default class App extends React.Component {
                       keyExtractor={item => item.id}
                       data={photos}
                       renderItem={({item}) =>
-                          <View>
-                              <View style={styles.postHeader}>
-                                  <Image source={require('./resources/images/instaPic.jpg')}
-                                         style={styles.profilePicture}/>
-                                  <Text>{item.user}</Text>
-                              </View>
-                              <Image source={require('./resources/images/instaPic.jpg')}
-                                     style={styles.postPicture}/>
-                          </View>
+                          <Post item={item}/>
                       }/>
         );
     }
@@ -46,20 +27,5 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
     container: {
         marginTop: 20,
-    },
-    postHeader: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        margin: 10,
-    },
-    profilePicture: {
-        width: 40,
-        height: 40,
-        marginRight: 10,
-        borderRadius: 20,
-    },
-    postPicture: {
-      width: width,
-      height: width,
     },
 });
