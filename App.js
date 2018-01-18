03a420e1a638c3ef54373c7fca8e718adeca8322
Rafael Mota <faelmota@gmail.com>
2018-01-18 20:15:22 -0300

    Add styleSheet and post header

--- App.js
+++ App.js
@@ -4,36 +4,62 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React from 'react';
 import {
     Text,
     View,
     Image,
     Dimensions,
-    FlatList
+    FlatList,
+    StyleSheet,
 } from 'react-native';
 
 const photos = [
     {id: 1, user: 'Rafael'},
     {id: 2, user: "Jorge"},
-    {id: 3, user: 'Irineu'}
+    {id: 3, user: 'Irineu'},
 ];
 
 const {width} = Dimensions.get('screen');
 
-export default class App extends Component {
+export default class App extends React.Component {
     render() {
         return (
-            <FlatList style={{marginTop: 20}}
+            <FlatList style={styles.container}
                       keyExtractor={item => item.id}
                       data={photos}
                       renderItem={({item}) =>
                           <View>
-                              <Text>{item.user}</Text>
+                              <View style={styles.postHeader}>
+                                  <Image source={require('./resources/images/instaPic.jpg')}
+                                         style={styles.profilePicture}/>
+                                  <Text>{item.user}</Text>
+                              </View>
                               <Image source={require('./resources/images/instaPic.jpg')}
-                                     style={{width: width, height: width}}/>
+                                     style={styles.postPicture}/>
                           </View>
                       }/>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 20,
+    },
+    postHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        margin: 10,
+    },
+    profilePicture: {
+        width: 40,
+        height: 40,
+        marginRight: 10,
+        borderRadius: 20,
+    },
+    postPicture: {
+      width: width,
+      height: width,
+    },
+});
