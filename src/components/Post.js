3d82b08e155dfabd78a8b9891a7a9ece4b15bb7a
Rafael Mota <faelmota@gmail.com>
2018-01-19 09:31:48 -0300

    Implementation of Like Feature

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,22 +5,51 @@ import {
     Image,
     Dimensions,
     StyleSheet,
+    TouchableOpacity,
 } from 'react-native';
 
 const {width} = Dimensions.get('screen');
 
 export default class Post extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            post: this.props.post,
+        };
+
+        this.like = this.like.bind(this);
+    }
+
+    like() {
+        const updatedPost = {
+            ...this.state.post,
+            likeada: !this.state.post.likeada,
+        };
+        this.setState({post: updatedPost});
+    }
+
+    loadLike(liked) {
+        return liked ? require('../../resources/images/s2-checked.png')
+            : require('../../resources/images/s2.png');
+    }
+
     render() {
-        const {post} = this.props;
+        const {post} = this.state;
         return (
             <View>
                 <View style={styles.postHeader}>
-                    <Image source={{uri: post.urlPerfil}}
-                           style={styles.profilePicture}/>
+                    <Image source={{uri: post.urlPerfil}} style={styles.profilePicture}/>
                     <Text>{post.loginUsuario}</Text>
                 </View>
-                <Image source={{uri: post.urlFoto}}
-                       style={styles.postPicture}/>
+                <Image source={{uri: post.urlFoto}} style={styles.postPicture}/>
+
+                <View style={styles.footer}>
+                    <TouchableOpacity onPress={this.like}>
+                        <Image source={this.loadLike(post.likeada)} style={styles.likeImage}/>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -41,5 +70,12 @@ const styles = StyleSheet.create({
     postPicture: {
         width: width,
         height: width,
+    },
+    footer: {
+        margin: 10,
+    },
+    likeImage: {
+        width: 40,
+        height: 40,
     },
 });
